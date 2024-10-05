1dd4b0c69a6975b9353357500461d0c9e19846f1
[calexd] <[caledarvs@gmail.com]>
2024-10-05 02:50:57 -0300

    Repair registration function for collection points: - Configure latitude and longitude to save and display to the user during the registration process of collection points.

--- src/context/usuariosContext.jsx
+++ src/context/usuariosContext.jsx
@@ -238,6 +238,7 @@ export const UsuariosContextProvider = ({ children }) => {
         cadastrarColeta,
         deleteData,
         editData,
+        getGeocoding
       }}
     >
       {children}
